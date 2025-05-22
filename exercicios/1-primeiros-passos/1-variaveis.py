07a969c00e612b2ebec2d6336e32d3e3e2db025f
migsrc <miguel_rcampos@outlook.com>
2025-05-22 12:09:53 +0000

    resolução exercício variaves

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,19 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'miguel' 
+
+profissao = "vendedor"
+
+objetivo = '''
+aprender python 
+e mudar 
+de profissão'''
 
 # Declare variáveis de cada tipo numérico
-
+numero_inteiro = 6
+salario = 1500.13
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
