5e76ed3fb686ca1784af08d02fb56b556454474c
migsrc <miguel_rcampos@outlook.com>
2025-05-22 13:34:27 +0000

    resolucao exercicio numerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,16 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 # Realize uma operação entre dados do tipo string e inteiro
-
+print ('miguel ' * 3)
 
 # Realize uma operação entre dados do tipo inteiro e float
+decimal = (1+2.75)
+print(decimal)
+print(int(decimal))
+print(round(decimal,1))
+print(round(decimal,0))
+print(f'Hoje paloma estudou {3+1} horas')
